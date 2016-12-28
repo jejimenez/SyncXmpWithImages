17822a42a29107fe5752d81f59da0c19d084f037
Jaime Jiménez <jimenez.ing.sis@gmail.com>
2016-12-28 01:32:38 -0500

    rebuiild to install as pkg 2

---
+++ pysync_image_metadata/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysync_images_metadata',
+      version='0.1',
+      description='Sync xmp metadata files with related jpg and nef images',
+      url='https://github.com/jejimenez/SyncXmpWithImages',
+      author='Jaime Jimenez',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['pysync_images_metadata'],
+      zip_safe=False)
